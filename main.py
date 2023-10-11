1c58af3a8518575f1b7cf08649f7d8a144ba1571
Artutev <artutev@gmail.com>
2023-10-11 20:11:17 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#1
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+2#
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+#3
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+#4
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+#5
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+#6
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+#7
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
